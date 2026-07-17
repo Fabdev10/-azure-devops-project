4d5421386db25d05057877c8c07b92f0e491ba49
Fabio Mazza <fabmaz91@gmail.com>
2026-07-17 12:06:08 +0200

    feat(aks): ripristinato outbound_type = userAssignedNATGateway nel network_profile

--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -57,6 +57,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
     service_cidr   = "172.16.0.0/16"
     dns_service_ip = "172.16.0.10"
+
+    outbound_type = "userAssignedNATGateway"
   }
 
   tags = local.common_tags
